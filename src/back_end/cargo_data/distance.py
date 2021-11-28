91904170fa6bf74f8f85735994c16b322e803289
KenHino <63284175+KenHino@users.noreply.github.com>
2021-11-28 14:44:26 +0900

    my work

--- src/back_end/cargo_data/distance.py
+++ src/back_end/cargo_data/distance.py
@@ -1,7 +1,29 @@
 import numpy as np
 import random
 from scipy.spatial import distance
-import pandas as pd
+from math import pi, atan2
+import pickle
+
+def circles_intersection_area(P1, r1, P2, r2):
+    x1, y1 = P1; x2, y2 = P2
+
+    dd = (x1 - x2)**2 + (y1 - y2)**2
+
+    if (r1 + r2)**2 <= dd:
+        return 0.0
+
+    if dd <= (r1 - r2)**2:
+        return pi*min(r1, r2)**2
+
+    p1 = (r1**2 - r2**2 + dd)
+    p2 = (r2**2 - r1**2 + dd)
+
+    S1 = r1**2 * atan2((4*dd*r1**2 - p1**2)**.5, p1)
+    S2 = r2**2 * atan2((4*dd*r2**2 - p2**2)**.5, p2)
+    S0 = (4*dd*r1**2 - p1**2)**.5 / 2
+
+    return S1 + S2 - S0
+
 
 
 def cargo_data(N_cargo=20,T_end=100):
@@ -12,40 +34,62 @@ def cargo_data(N_cargo=20,T_end=100):
 
     coordinate = [(0,0)] + [tuple(random.randint(-10,10) for i in range(2)) for j in range(N_cargo-1)] # (21 km, 21 km)
     arrive_time = [ALL] + [AM]*(N_cargo//4) + [PM]*(N_cargo//4) + [ALL]*(N_cargo - 1 - N_cargo//4 - N_cargo//4)
-    distance_matrix = [[ 0 for _ in range(N_cargo) for _ in range(N_cargo)]]
+    distance_matrix = [[ 0 for _ in range(N_cargo)] for _ in range(N_cargo)]
     time_matrix = [[[0 for _ in range(T_end)] for i_cargo in range(N_cargo)] for j_cargo in range(N_cargo)]
 
-    def rush(time,x,y):
+    def rush(time,coord_1,coord_2):
         '''関数を書く 特定の場所のみなど'''
-        AREA_1 = 5 # random.randint(-10,10)
-        AREA_2 = 5 # random.randint(-10,10)
-        rush_time = T_end - 20
-        if time < rush_time and AREA_1 < x and AREA_1 < y:
-            dum = time + random.randint(1,10)
-        elif time < rush_time and x < AREA_2 and AREA_1 < y:
-            dum = time - random.randint(1,10)
-        elif time < rush_time and x < AREA_2 and y < AREA_2:
-            dum = time + random.randint(1,10)
-        elif time < rush_time and AREA_1 < x and y < AREA_2:
-            dum = time - random.randint(1,10)
+        rushtime_1 = [0,T_end//5]
+        rushtime_2 = [T_end//5*4, T_end]
+
+
+        if rushtime_1[0] <= time <=rushtime_1[1] or rushtime_2[0] <= time <= rushtime_2[1]:
+            coef = 0.5
+            P1 = [(coord_1[0] + coord_2[0])/2, (coord_1[1] + coord_2[1])/2]
+            r1 = distance.euclidean(coord_1, coord_2)/2
+            P2 = [0,0]
+            r2 = 5
+            additional_time = circles_intersection_area(P1, r1, P2, r2)*coef
         else:
-            dum = time
-        return dum
+            additional_time = 0
+
+        return additional_time
 
 
+    max_time = 0
     for i_cargo in range(N_cargo):
         for j_cargo in range(N_cargo):
             if i_cargo == j_cargo:
                 continue
             # 距離 + なにかにする
-            d = int(distance.euclidean(coordinate[i_cargo], coordinate[j_cargo]))
-            d += abs(int(np.random.normal(loc = 5, scale = 0)))
+            d = (distance.euclidean(coordinate[i_cargo], coordinate[j_cargo]))
+            d += abs((np.random.normal(loc = 5, scale = 0)))
             distance_matrix[i_cargo][j_cargo] = d
             for time in range(T_end):
-                time_matrix[i_cargo][j_cargo][time] = (distance_matrix[i_cargo][j_cargo] / verocity) + rush(time,coordinate[i_cargo],coordinate[j_cargo])*random.random()
+                time_matrix[i_cargo][j_cargo][time] = int((distance_matrix[i_cargo][j_cargo] / verocity)\
+                        + rush(time,coordinate[i_cargo],coordinate[j_cargo]))//7
+                max_time = max(max_time, time_matrix[i_cargo][j_cargo][time])
 
+    max_time *= N_cargo
+    zeros = [0 for _  in range(T_end, max_time)]
+    for i_cargo in range(N_cargo):
+        for j_cargo in range(N_cargo):
+            time_matrix[i_cargo][j_cargo] += zeros
     # df = pd.DataFrame(np.array(distance_matrix))
     # df = pd.read_csv('test_distance.csv', index_col=0)
     return coordinate, arrive_time, distance_matrix, time_matrix
 
-cargo_data()
+if __name__ == '__main__':
+    coordinate, arrive_time, distance_matrix, time_matrix = cargo_data(N_cargo=20)
+    #print(coordinate)
+    #print(arrive_time)
+    #print(distance_matrix)
+    print(time_matrix)
+    with open('coordinate.binaryfile', 'wb') as c:
+        pickle.dump(coordinate, c)
+    with open('arrive_time.binaryfile', 'wb') as a:
+        pickle.dump(arrive_time, a)
+    with open('distance_matrix.binaryfile', 'wb') as d:
+        pickle.dump(distance_matrix, d)
+    with open('time_matrix.binaryfile', 'wb') as t:
+        pickle.dump(time_matrix, t)
--- src/back_end/main.py
+++ src/back_end/main.py
@@ -1,19 +1,32 @@
-import numpy as np
 from simulator_cls import simulator
+import pickle
 
-coordinate = [(0,0),(1,1),(2,2),(3,3)]
-distance_matrix = [[0, 1, 2, 3],
-                   [1, 0, 1, 2],
-                   [2, 1, 0, 1],
-                   [3, 2, 1, 0]]
-time_matrix = [[[0]*20, [1]*20, [4]*20, [9]*20],
-               [[1]*20, [0]*20, [1]*20, [4]*20],
-               [[4]*20, [1]*20, [0]*20, [1]*20],
-               [[9]*20, [4]*20, [1]*20, [0]*20]]
+#coordinate = [(0,0),(1,1),(2,2),(3,3)]
+#distance_matrix = [[0, 1, 2, 3],
+#                   [1, 0, 1, 2],
+#                   [2, 1, 0, 1],
+#                   [3, 2, 1, 0]]
+#time_matrix = [[[0]*20, [1]*20, [4]*20, [9]*20],
+#               [[1]*20, [0]*20, [1]*20, [4]*20],
+#               [[4]*20, [1]*20, [0]*20, [1]*20],
+#               [[9]*20, [4]*20, [1]*20, [0]*20]]
+#
+#arrive_time = [[-1,100], [-1,10], [10,10], [-1,20]]
 
-arrive_time = [[-1,100], [-1,10], [-1,10], [-1,20]]
+with open('cargo_data/coordinate.binaryfile', 'rb') as c:
+    coordinate = pickle.load(c)
+with open('cargo_data/distance_matrix.binaryfile', 'rb') as d:
+    distance_matrix = pickle.load(d)
+with open('cargo_data/time_matrix.binaryfile', 'rb') as t:
+    time_matrix = pickle.load(t)
+with open('cargo_data/arrive_time.binaryfile', 'rb') as a:
+    arrive_time = pickle.load(a)
 
 
-sim = simulator(coordinate, distance_matrix, time_matrix, arrive_time, max_iter=20)
-sim.optimize(print_out=False)
-print(sim.output())
+
+sim = simulator(coordinate, distance_matrix, time_matrix, arrive_time, max_iter=10**5, alpha=0.5)
+sim.optimize()
+cost, time_list, order = sim.output()
+print('cost',cost)
+print('time',time_list)
+print('order', order)
