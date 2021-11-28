3560e78459fcd9ef40d2bcd6601b548912d254e0
KenHino <63284175+KenHino@users.noreply.github.com>
2021-11-28 10:53:29 +0900

    distance

--- src/back_end/cargo_data/distance.py
+++ src/back_end/cargo_data/distance.py
@@ -1,51 +1,54 @@
 import numpy as np
-from numpy import core
-import numpy
-import pandas as pd
 import random
 from scipy.spatial import distance
+import pandas as pd
 
-N_cargo = 10
 
-MORNING = 180 # 朝9時から12時までの180分
-AFTERNOON = 480 # 12時から20時までの480分
+def cargo_data(N_cargo=20,T_end=100):
+    AM = [-1, T_end//2] # 朝9時から12時までの180分
+    PM = [T_end//2,T_end] # 12時から20時までの480分
+    ALL = [-1, T_end]
+    verocity = 10 / T_end * 7 # 10 km / h
 
-coordinate = [[random.randint(1,10) for i in range(2)] for j in range(N_cargo)]
-coordinate.insert(0,[0,0]) # 原点の追加
+    coordinate = [(0,0)] + [tuple(random.randint(-10,10) for i in range(2)) for j in range(N_cargo-1)] # (21 km, 21 km)
+    arrive_time = [ALL] + [AM]*(N_cargo//4) + [PM]*(N_cargo//4) + [ALL]*(N_cargo - 1 - N_cargo//4 - N_cargo//4)
+    distance_matrix = [[ 0 for _ in range(N_cargo) for _ in range(N_cargo)]]
+    time_matrix = [[[0 for _ in range(T)] for i_cargo in range(N_cargo)] for j_cargo in range(N_cargo)]
 
-distance_matrix = []
+    def rush(time):
+        '''関数を書く'''
+        dum = time
+        return dum
 
-for i in range(len(coordinate)-1):
-    distance_array = []
-    distance_matrix.append(distance_array)
-    for j in range(len(coordinate)-1):
-        # 距離 + なにかにする
-        a = int(distance.euclidean(coordinate[i], coordinate[j]))
-        if not i == j and random.random() < 0.9 :
-            a += abs(int(np.random.normal(loc = 5, scale = 0)))
-        distance_array.append(a)
 
-time_matrix = []
+    for i_cargo in range(N_cargo):
+        for j_cargo in range(N_cargo):
+            if i_cargo == j_cargo:
+                continue
+            # 距離 + なにかにする
+            d = int(distance.euclidean(coordinate[i_cargo], coordinate[j_cargo]))
+            d += abs(int(np.random.normal(loc = 5, scale = 0)))
+            distance_matrix[i_cargo][j_cargo] = d
 
-for i in range(len(coordinate)-1):
-    time_array = []
-    time_matrix.append(time_array)
-    for j in range(len(coordinate)-1):
-        time_array.append(distance_matrix[i][j]* 10)
+            for time in range(T_end):
+                time_matrix[i_cargo][j_cargo][time] = (distance_matrix[i][j] / verocity) + rush(time)*random.random()
 
-#for i in range(N_cargo):
-    #prob = random.random()
-    #if prob < 0.2:
-        #pass
-    #elif prob < 0.4:
-        #pass
-    #else:
-# [[random.randint(1,10) for i in range(2)]
 
-# print(coordinate)
-# print(distance_matrix)
-print(numpy.array(distance_matrix))
-print(numpy.array(time_matrix))
-df = pd.DataFrame(numpy.array(distance_matrix))
-print(df)
-# df = pd.read_csv('test_distance.csv', index_col=0)
+
+    #for i in range(N_cargo):
+        #prob = random.random()
+        #if prob < 0.2:
+            #pass
+        #elif prob < 0.4:
+            #pass
+        #else:
+    # [[random.randint(1,10) for i in range(2)]
+
+    # print(coordinate)
+    # print(distance_matrix)
+    print(numpy.array(distance_matrix))
+    print(numpy.array(time_matrix))
+    df = pd.DataFrame(numpy.array(distance_matrix))
+    print(df)
+    # df = pd.read_csv('test_distance.csv', index_col=0)
+    return coordinate, arrive_time, distance_matrix, time_matrix
